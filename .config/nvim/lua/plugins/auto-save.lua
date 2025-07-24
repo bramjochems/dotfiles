19294cc3a9c93225c2696d72e9b4bc4afac7b659
Bram Jochems <bram@riskatwork.nl>
2025-07-24 21:43:26 +0200

    Autosave plugin for neovim

---
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -0,0 +1,31 @@
+return {
+  "pocco81/auto-save.nvim",
+  event = { "InsertLeave", "TextChanged" },
+  keys = {
+    {
+      "<leader>ua",
+      function()
+        require("auto-save").toggle()
+        vim.notify("Auto-save toggled")
+      end,
+      desc = "Toggle Auto-Save",
+    },
+  },
+  opts = {
+    enabled = true,
+    execution_message = {
+      message = function()
+        return "💾 saved at " .. vim.fn.strftime("%H:%M:%S")
+      end,
+      dim = 0.18,
+      cleaning_interval = 1250,
+    },
+    debounce_delay = 1350, -- time in ms after last change before save
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+      return fn.getbufvar(buf, "&modifiable") == 1
+        and utils.not_in(fn.getbufvar(buf, "&filetype"), { "NvimTree", "TelescopePrompt" })
+    end,
+  },
+}
