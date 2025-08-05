ecc957adb7e77bedf2d2bbb394ab444d80b27908
Bram Jochems <bram@riskatwork.nl>
2025-08-05 23:10:56 +0200

    WIP

--- .bash_aliases
+++ .bash_aliases
@@ -7,3 +7,4 @@ alias ll='ls -alhF'
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+alias npm-update='npm install -g $(npm outdated -g --parseable --depth=0 | cut -d: -f4)'
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,14 +1,31 @@
-return (vim.g.vscode and {}) or {
-  "folke/snacks.nvim",
-  -- opts will be merged with the parent spec
-  opts = {
-    picker = {
-      sources = {
-        explorer = {
-          hidden = true,
-          ignored = true,
+return (vim.g.vscode and {})
+  or {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
         },
       },
     },
-  },
-}
+    config = function(_, opts)
+      -- Apply the existing Snacks options
+      require("snacks").setup(opts)
+
+      -- ✅ Add your custom keymap (Capital C) to set cwd from selected file
+      vim.keymap.set("n", "<leader>C", function()
+        local explorer = require("snacks.explorer")
+        local entry = explorer.get_selected_entry and explorer.get_selected_entry()
+        if not entry then
+          vim.notify("No file selected in Snacks Explorer", vim.log.levels.WARN)
+          return
+        end
+        local dir = vim.fn.fnamemodify(entry.path, ":h")
+        vim.cmd("cd " .. dir)
+        vim.notify("Changed cwd to: " .. dir)
+      end, { desc = "Change CWD to selected file in Snacks Explorer" })
+    end,
+  }
